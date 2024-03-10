200e472213834b125244cf7bf536c2f658211f99
dealbisac <chand.dipendra19@gmail.com>
2024-03-10 12:59:17 +0100

    2: add auth default layout

--- app/(root)/layout.ts
+++ app/(root)/layout.ts
@@ -3,7 +3,7 @@ import React from 'react'
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
-      <div className="root-container">
+    <div className="root-container">
         <div className="wrapper">
           {children}
         </div>
