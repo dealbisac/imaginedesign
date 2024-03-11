afa9d2933bdb46a6f30153ba222a4ed3e3d8927d
dealbisac <chand.dipendra19@gmail.com>
2024-03-11 17:38:32 +0100

    2: update root layout

--- app/(auth)/layout.ts
+++ app/(auth)/layout.ts
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="auth">{children}</main>
+    <main className="auth">
+      {children}
+    </main>
   )
 }
 
