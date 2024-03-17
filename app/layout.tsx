ef9750627b54bf1550fa856e2f5571bc460188d8
dealbisac <chand.dipendra19@gmail.com>
2024-03-17 14:34:39 +0100

    2: wrap clerkprovider to base layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
+import { ClerkProvider } from '@clerk/nextjs'
+
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
@@ -20,10 +22,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={cn("font-IBMPlex antialiased IBMPlex.variable")}>
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
